7ffb5d541d7dbc1465a541e71a249c48d2b6e627
Krishna Aggarwal <krishnaaggarwal925@gmail.com>
2025-07-05 19:05:45 +0530

    improved UI

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const toggle = document.getElementById("toggle");
 const nav = document.getElementById("nav");
-toggle.onclick = function(){
-  toggle.classList.toggle('press');
-  nav.classList.toggle('dark');
-  document.body.classList.toggle('dark');
-}
+toggle.onclick = function () {
+  toggle.classList.toggle("press");
+  nav.classList.toggle("dark");
+
+  const isDark = document.body.classList.toggle("dark");
+
+  if (!isDark) {
+    document.body.classList.add("light");
+  } else {
+    document.body.classList.remove("light");
+  }
+};
