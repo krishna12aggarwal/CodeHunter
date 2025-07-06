6c3c7480695655e47b4b055f8b93e9e048a0ac52
Krishna Aggarwal <krishnaaggarwal925@gmail.com>
2025-07-06 18:27:20 +0530

    Ui

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const toggle = document.getElementById("toggle");
 const nav = document.getElementById("nav");
+const loginBtn = document.getElementById("login");
+const log = document.getElementById("screen");
+const clos = document.getElementById("close");
+const a = document.getElementById("a");
+
 toggle.onclick = function () {
   toggle.classList.toggle("press");
   nav.classList.toggle("dark");
 
   const isDark = document.body.classList.toggle("dark");
-
-  if (!isDark) {
-    document.body.classList.add("light");
-  } else {
-    document.body.classList.remove("light");
-  }
+  document.body.classList.toggle("light", !isDark);
 };
+loginBtn.addEventListener("click", () => {
+  log.classList.add("show"); 
+});
+a.addEventListener("click", () => {
+  log.classList.add("show");
+});
+clos.addEventListener("click", () => {
+  log.classList.remove("show"); 
+});
